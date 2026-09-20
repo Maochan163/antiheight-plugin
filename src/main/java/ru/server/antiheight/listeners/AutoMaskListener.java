757df35ac8452a3685b5e2aa3577e1a31074a85d
Maochan163 <maohuoshi62@gmail.com>
2026-09-20 13:11:51 +0200

    Clean up imports in AutoMaskListener.java
    Removed unused import for PlayerArmorChangeEvent and SkullMeta.

--- src/main/java/ru/server/antiheight/listeners/AutoMaskListener.java
+++ src/main/java/ru/server/antiheight/listeners/AutoMaskListener.java
@@ -1,12 +1,11 @@
 package ru.server.antiheight.listeners;
 
+import com.destroystokyo.paper.event.player.PlayerArmorChangeEvent;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerArmorChangeEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.SkullMeta;
 import ru.server.antiheight.items.CustomItemFactory;
 
 /**
